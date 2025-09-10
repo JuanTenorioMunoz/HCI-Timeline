be9dcdbd0fabe4e3b4aadf7e4ddd78103b953107
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-09-09 22:04:46 -0500

    add spline example

---
+++ src/components/Spline/Spline.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Home() {
+  return (
+    <Spline scene="https://prod.spline.design/a9PQzMAFbNBAbNrn/scene.splinecode" />
+  );
+}
